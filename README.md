b08fd30faf8daa2699205fc31616aa7fc800e264
Shaik <shaikarshad1792004@gmail.com>
2024-07-15 22:47:56 +0530

    added stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an Personal Learning Data structures and algorithms in C++ 
+
---
+++ io.cpp
@@ -0,0 +1,22 @@
+// The skeleton of the C++ is 
+
+// #include<iostream>
+// #include is an thing that is including the Iostream library
+
+// we can include multiple libraries like 
+// #include<math.h> but, we include an singular library that all of the librarys it is a bit slow but, preferrable to use everywhere 
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+// this is where you codes part gets to start from 
+int main() {
+  //in here we can use 
+  int x, y;
+  cin >> x >> y;
+  // to store values we use cin and followed by the value and 
+  cout << "The Value of x + y : " << x+y; 
+  // to print something we use Cout and followed by the value 
+  // In here we would be using the std::cin...., std::cout...., but, we are using something called namespace for this stuff.
+
+}
