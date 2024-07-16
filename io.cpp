c81ee9633ce3eedaa7df52feed53207875f5b81f
Shaik <shaikarshad1792004@gmail.com>
2024-07-16 21:48:11 +0530

    testing change

--- io.cpp
+++ io.cpp
@@ -10,28 +10,43 @@
 
 using namespace std; 
 
-// this is where you codes part gets to start from 
-int main() {
-  //in here we can use 
-  int x, y;
-  cin >> x >> y;
-  // to store values we use cin and followed by the value and 
-  cout << "The Value of x + y : " << x+y; 
-  // to print something we use Cout and followed by the value 
-  // In here we would be using the std::cin...., std::cout...., but, we are using something called namespace for this stuff.
+// // this is where you codes part gets to start from 
+// int main() {
+//   //in here we can use 
+//   int x, y;
+//   cin >> x >> y;
+//   // to store values we use cin and followed by the value and 
+//   cout << "The Value of x + y : " << x+y; 
+//   // to print something we use Cout and followed by the value 
+//   // In here we would be using the std::cin...., std::cout...., but, we are using something called namespace for this stuff.
 
-}
+// }
 
 int main() {
 
   //datatypes
   
   // integer  range : -2,147,483,648 to 2,147,483,647  can only store up this value
-  int x = 1;
+  int A = 1;
 
   // long  range :
-  // Signed long long: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-	// Unsigned long long: 0 to 18,446,744,073,709,551,615
-  long x = 15;
+  // Signed long: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+  // Unsigned long long: 0 to 18,446,744,073,709,551,615
+  long B = 15;
+	
+  long long C = 15; 
+
+  //float, double
+
+  float D = 5.6;
+  
+  double F = 5.77;
+
+  //string 
+  string c1, c2;
+
+  cin >> c1 >> c2;
+
+  cout << c1 << " " << c2;
   return 0;
 }
