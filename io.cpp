b0b6a52f3e339fb80f50a415aa7634e9b72606ef
Shaik <shaikarshad1792004@gmail.com>
2024-07-16 20:41:03 +0530

    -m

--- io.cpp
+++ io.cpp
@@ -6,6 +6,7 @@
 // we can include multiple libraries like 
 // #include<math.h> but, we include an singular library that all of the librarys it is a bit slow but, preferrable to use everywhere 
 #include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std; 
 
@@ -19,4 +20,18 @@ int main() {
   // to print something we use Cout and followed by the value 
   // In here we would be using the std::cin...., std::cout...., but, we are using something called namespace for this stuff.
 
+}
+
+int main() {
+
+  //datatypes
+  
+  // integer  range : -2,147,483,648 to 2,147,483,647  can only store up this value
+  int x = 1;
+
+  // long  range :
+  // Signed long long: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+	// Unsigned long long: 0 to 18,446,744,073,709,551,615
+  long x = 15;
+  return 0;
 }
